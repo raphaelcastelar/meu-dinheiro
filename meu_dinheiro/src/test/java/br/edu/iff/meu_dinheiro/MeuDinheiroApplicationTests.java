9199a58ce998bf44eeb11ece7be3dbee78dad1e9
Raphael Castelar <castelaraphael@gmail.com>
2025-07-29 13:15:43 -0300

    TR02-Preparacao Ambiente

---
+++ meu_dinheiro/src/test/java/br/edu/iff/meu_dinheiro/MeuDinheiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.meu_dinheiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuDinheiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
