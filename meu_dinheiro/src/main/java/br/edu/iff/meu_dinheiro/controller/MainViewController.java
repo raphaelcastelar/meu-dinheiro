9dd20f39b4f86a79425aacd5189008efbdab5eb6
Raphael Castelar <castelaraphael@gmail.com>
2025-08-09 21:32:15 -0300

    aula 04/08

--- meu_dinheiro/src/main/java/br/edu/iff/meu_dinheiro/controller/MainViewController.java
+++ meu_dinheiro/src/main/java/br/edu/iff/meu_dinheiro/controller/MainViewController.java
@@ -2,17 +2,29 @@ package br.edu.iff.meu_dinheiro.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.ui.Model;
+
+
 
 
 @Controller
 @RequestMapping(path = "/principal")
 public class MainViewController {
     
-    @GetMapping("/")
-    public String getHomePage() {
+    @GetMapping("/{id}")
+    public String getHomePage(@PathVariable("id") String id,@RequestParam ("nome") String nome, Model model) {
+        model.addAttribute("id", id);
+        model.addAttribute("nome", nome);
+        model.addAttribute("saldo", 100.00);
+        model.addAttribute("gastos", 400.00);
+        model.addAttribute("metaEconomia", 500.00);
+        
         return "index.html";
-    }
+    }}
     
-}
+
