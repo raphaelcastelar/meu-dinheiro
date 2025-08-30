d52763cc336b402b03a4be3da7f309f0054bb858
Raphael Castelar <castelaraphael@gmail.com>
2025-08-29 22:51:12 -0300

    att entrega p1 - htmls, pom, properties

--- meu_dinheiro/src/main/java/br/edu/iff/meu_dinheiro/entities/Despesa.java
+++ meu_dinheiro/src/main/java/br/edu/iff/meu_dinheiro/entities/Despesa.java
@@ -3,14 +3,14 @@ package br.edu.iff.meu_dinheiro.entities;
 import java.io.Serializable;
 
 
-import jakarta.persistence.Entity;
+//import jakarta.persistence.Entity;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 
-@Entity
+//@Entity
 public class Despesa implements Serializable {
 
 
--- meu_dinheiro/src/main/java/br/edu/iff/meu_dinheiro/service/CategoriaService.java
+++ meu_dinheiro/src/main/java/br/edu/iff/meu_dinheiro/service/CategoriaService.java
@@ -1,6 +1,6 @@
 package br.edu.iff.meu_dinheiro.service;
 
-import br.edu.iff.meu_dinheiro.entities.categoria;
+import br.edu.iff.meu_dinheiro.entities.Categoria;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,17 +8,17 @@ import java.util.List;
 
 @Service
 public class CategoriaService {
-    private List<categoria> categorias = new ArrayList<>();
+    private List<Categoria> categorias = new ArrayList<>();
     private Long nextId = 1L;
 
     public CategoriaService() {
-        categorias.add(new categoria(1L, "Alimentação"));
-        categorias.add(new categoria(2L, "Lazer"));
-        categorias.add(new categoria(3L, "Contas Fixas"));
+        categorias.add(new Categoria(1L, "Alimentação"));
+        categorias.add(new Categoria(2L, "Lazer"));
+        categorias.add(new Categoria(3L, "Contas Fixas"));
     }
 
-    public List<categoria> findAll() { return new ArrayList<>(categorias); }
-    public void save(categoria categoria) {
+    public List<Categoria> findAll() { return new ArrayList<>(categorias); }
+    public void save(Categoria categoria) {
         if (categoria.getId() == null) {
             categoria.setId(nextId++);
             categorias.add(categoria);
